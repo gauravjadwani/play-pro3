8b1f3e26ddd8f92272ed5b1a89d2e40ec579421e
gauravjadwani <gaurav.jadwani@hotmail.com>
2016-11-21 13:15:57 +0530

    add tasks with the notifications completed

--- controllers/add_project.php
+++ controllers/add_project.php
@@ -29,6 +29,7 @@ include_once '../controllers/init_session.php';
    
        $r->hMset('group:'.$group_id, array('name' => $name_group,'created_on'=>$date,'closed_on'=>'live'));
    $r->zadd("group_permissions:".$group_id,'1',$user_id);
+   $r->zadd("state:projects",'0',$project_id);
    $r->zadd("notifications:".$user_id,$current_time,"you added yourself as the group owner in the ".$name_group);
  
     
@@ -88,7 +89,7 @@ for($i=0;$i<sizeof($split_email);$i++)
      $r->zadd("notifications:".$email_userid,$current_time,"you were added in the project".$name_project);
      
       $r->zadd("group_permissions:".$group_id,'3',$email_userid);
- $r->zadd("notifications:".$email_userid,$current_time,"you have been added as the group modifier in the ".$name_group."by the ".$name);
+ $r->zadd("notifications:".$email_userid,$current_time,"you have been added as the group read-only in the ".$name_group."by the ".$name);
      
      
     
@@ -110,6 +111,7 @@ $r->hincrby('parent','group_id',1);
       $r->zadd("notifications:".$user_id,$current_time,"you created the project ".$name_project."with the group id ".$form_group_id);
      
       $r->sadd("projects_group:".$form_group_id,$project_id);
+      $r->zadd("state:projects",'0',$project_id);
     
     $list_modify=$_POST['list_members_modify'];
     $list_readonly=$_POST['list_members_readonly'];
