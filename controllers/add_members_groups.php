9a4caacb32dcb894962d38f048f4468e65ab662d
gauravjadwani <gaurav.jadwani@hotmail.com>
2016-11-22 21:10:59 +0530

    updated notification

--- controllers/add_members_groups.php
+++ controllers/add_members_groups.php
@@ -9,6 +9,7 @@ if(isset($_POST['group_id']))
         $list_modify=$_POST['list_members_modify'];
     $list_readonly=$_POST['list_members_readonly'];
     $group_id=$_POST['group_id'];
+    $group_id_name=$r->hget("group:".$group_id,'name');
     $current_time=time();
     echo $group_id."<br>";
     if(!empty($list_modify))
@@ -19,10 +20,11 @@ for($i=0;$i<sizeof($split_email);$i++)
 {
      //echo 'list_modify:'.$split_email[$i].'<br>';
     $email_userid=$r->hget("email:user",$split_email[$i]);
-    echo $email_userid;
+    //echo $email_userid;
+    $email_userid_name=$r->hget("user:".$email_userid,'name');
      //$r->sadd("projects:".$email_userid,$project_id);
      $r->zadd("notifications:".$email_userid,$current_time,"you were added in the project  by ".$name);
-     $r->zadd("notifications:".$user_id,$current_time,"you added ".$email_userid." in the project ");
+     $r->zadd("notifications:".$user_id,$current_time,"you added ".$email_userid_name." in the group ".$group_id_name);
      
          
         
@@ -47,10 +49,11 @@ for($i=0;$i<sizeof($split_email);$i++)
    
  
   $email_userid=$r->hget("email:user",$split_email[$i]);
+   $email_userid_name=$r->hget("user:".$email_userid,'name');
      //$r->sadd("projects:".$email_userid,$project_id);
      $r->zadd("notifications:".$email_userid,$current_time,"you were added in the project ".$name_project." by ".$user_name);
-      $r->zadd("notifications:".$user_id,$current_time,"you added ".$email_userid." in the project ".$name_project);
-      $r->zadd("group_permissions:".$group_id,'3',$email_userid);
+          $r->zadd("notifications:".$user_id,$current_time,"you added ".$email_userid_name." in the project ");
+     $r->zadd("group_permissions:".$group_id,'3',$email_userid);
  //$r->zadd("notifications:".$email_userid,$current_time,"you have been added as the group read-only in the ".$name_group."by the ".$name);
      
      
